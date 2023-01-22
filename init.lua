1c732c3304256193b3984ced88722c65e94d4e68
Sasank Thapa <sasank.thapa2000@gmail.com>
2023-01-21 22:19:09 -0600

    lsp zero woahhhh

--- init.lua
+++ init.lua
@@ -3,17 +3,33 @@ vim.opt.relativenumber = true
 vim.cmd([[packadd packer.nvim]])
 require("packer").startup(function()
   use("wbthomason/packer.nvim")
-  use('hrsh7th/cmp-nvim-lsp')
   use("morhetz/gruvbox")
   use("nvim-treesitter/nvim-treesitter")
   use {
     'nvim-lualine/lualine.nvim',
     requires = { 'kyazdani42/nvim-web-devicons', opt = true }
   }
-  use("neovim/nvim-lspconfig")
-  use('hrsh7th/nvim-cmp')
-  use('L3MON4D3/LuaSnip')
-  use('saadparwaiz1/cmp_luasnip')
+  use {
+  'VonHeikemen/lsp-zero.nvim',
+  requires = {
+    -- LSP Support
+    {'neovim/nvim-lspconfig'},
+    {'williamboman/mason.nvim'},
+    {'williamboman/mason-lspconfig.nvim'},
+
+    -- Autocompletion
+    {'hrsh7th/nvim-cmp'},
+    {'hrsh7th/cmp-buffer'},
+    {'hrsh7th/cmp-path'},
+    {'saadparwaiz1/cmp_luasnip'},
+    {'hrsh7th/cmp-nvim-lsp'},
+    {'hrsh7th/cmp-nvim-lua'},
+
+    -- Snippets
+    {'L3MON4D3/LuaSnip'},
+    {'rafamadriz/friendly-snippets'},
+  }
+}
 
   --prettier
   use('jose-elias-alvarez/null-ls.nvim')
